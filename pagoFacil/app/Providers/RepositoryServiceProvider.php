213321ab3d15cef109fee71e53fa141edc807fc9
Angel Barrientos <uetiko@gmail.com>
2019-09-16 12:49:52 -0500

    Added new repository in the service provider

--- pagoFacil/app/Providers/RepositoryServiceProvider.php
+++ pagoFacil/app/Providers/RepositoryServiceProvider.php
@@ -3,6 +3,10 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Uetiko\Prueba\Backend\Address\Domain\Interfaces\AddressRepository;
+use Uetiko\Prueba\Backend\Country\Infrastructure\CountryRepository;
+use Uetiko\Prueba\Backend\Country\Domain\Interfaces\CountryRepository as
+    CountryRepositoryInterface;
 use Uetiko\Prueba\Backend\CreditCard\Infrastructure\CreditCardRepository;
 use Uetiko\Prueba\Backend\CreditCard\Domain\Interfaces\CreditCardRepositoryInterfaces;
 use Uetiko\Prueba\Backend\User\Infrastructure\UserRepository;
@@ -25,11 +29,27 @@ class RepositoryServiceProvider extends ServiceProvider
         $this->app->singleton(
             UserRepository::class, function ($app) {
                 return new UserRepository();
-        }
+            }
+        );
+        $this->app->singleton(
+            CountryRepository::class, function ($app) {
+                return new CountryRepository();
+            }
+        );
+        $this->app->bind(
+            AddressRepository::class, function ($app) {
+                return new AddressRepository(
+                    $this->app->make(CountryRepositoryInterface::class)
+                );
+            }
         );
         $this->app->bind(
             CreditCardRepositoryInterfaces::class,
             CreditCardRepository::class
+        );
+        $this->app->bind(
+            CountryRepositoryInterface::class,
+            CountryRepository::class
         );
     }
 
