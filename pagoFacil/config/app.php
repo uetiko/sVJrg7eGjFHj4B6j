07b1d9b1ac618fb99f709ef4edfc07d503b4180e
Angel Barrientos <uetiko@gmail.com>
2019-09-16 15:56:58 -0500

    a new servide provider is registed.

--- pagoFacil/config/app.php
+++ pagoFacil/config/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Providers\RepositoryServiceProvider;
+
 return [
 
     /*
@@ -174,6 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        RepositoryServiceProvider::class
 
     ],
 
